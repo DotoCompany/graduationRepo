16fe1e913017793744847a198edf02cb9ddfc3e6
kwongiho <shng10503@naver.com>
2016-03-18 17:27:20 +0900

    add DBConnection

---
+++ src/model/DBConnection.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class DBConnection {
+	private Connection conn;
+	public static final DBConnection dbConnection;
+	static {
+		dbConnection = new DBConnection();
+	}
+	public Connection getConn() {
+		return conn;
+	}
+	public static DBConnection getInstance() {
+		return dbConnection;
+	}
+	private DBConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/graduationRepo","root","0000");
+			conn.setAutoCommit(false);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	public static void main(String [] args) {
+		DBConnection.getInstance();
+	}
+}
